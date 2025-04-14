1231053d2c4414517320bc2d3088c26bdda80397
prabirsekhri <prabirsekhri@gmail.com>
2025-04-14 12:23:13 -0400

    refactored kms policy and restricted permissions

---
+++ examples/sftp-public-endpoint-service-managed-S3/kms.tf
@@ -0,0 +1,106 @@
+###################################################################
+# KMS key and policies for Transfer Server
+###################################################################
+
+# KMS Key resource
+resource "aws_kms_key" "transfer_family_key" {
+  description             = "KMS key for encrypting S3 bucket and cloudwatch log group"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+
+  tags = {
+    Purpose = "Transfer Family Encryption"
+  }
+}
+
+# KMS Key Alias
+resource "aws_kms_alias" "transfer_family_key_alias" {
+  name          = "alias/transfer-family-key"
+  target_key_id = aws_kms_key.transfer_family_key.key_id
+}
+
+# KMS Key Policy
+resource "aws_kms_key_policy" "transfer_family_key_policy" {
+  key_id = aws_kms_key.transfer_family_key.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "Enable Limited Admin Permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action = [
+          "kms:Create*",
+          "kms:Describe*",
+          "kms:Enable*",
+          "kms:List*",
+          "kms:Put*",
+          "kms:Update*",
+          "kms:Revoke*",
+          "kms:Disable*",
+          "kms:Get*",
+          "kms:Delete*",
+          "kms:ScheduleKeyDeletion",
+          "kms:CancelKeyDeletion"
+        ]
+        Resource = aws_kms_key.transfer_family_key.arn
+      },
+      {
+        Sid    = "Allow CloudWatch Logs"
+        Effect = "Allow"
+        Principal = {
+          Service = "logs.${var.aws_region}.amazonaws.com"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:Describe*"
+        ]
+        Resource = aws_kms_key.transfer_family_key.arn
+      },
+      {
+        Sid    = "Allow Transfer and S3 Service"
+        Effect = "Allow"
+        Principal = {
+          Service = [
+            "transfer.amazonaws.com",
+            "s3.amazonaws.com"
+          ]
+        }
+        Action = [
+          "kms:Decrypt",
+          "kms:DescribeKey",
+          "kms:Encrypt",
+          "kms:GenerateDataKey*",
+          "kms:ReEncrypt*"
+        ]
+        Resource = aws_kms_key.transfer_family_key.arn
+      },
+      {
+        Sid    = "Allow Transfer Family User Roles"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action = [
+          "kms:Decrypt",
+          "kms:GenerateDataKey"
+        ]
+        Resource = aws_kms_key.transfer_family_key.arn
+        Condition = {
+          StringLike = {
+            "aws:PrincipalArn": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/transfer-user-*"
+          }
+          StringEquals = {
+            "aws:PrincipalType": "AssumedRole"
+          }
+        }
+      }
+    ]
+  })
+}
+
