83dccd3d3994f54fad7190d4719e22d8b2e375ff
prabirsekhri <prabirsekhri@gmail.com>
2025-04-14 12:55:56 -0400

    modified KMS policy for IAM user permissions

--- examples/sftp-public-endpoint-service-managed-S3/kms.tf
+++ examples/sftp-public-endpoint-service-managed-S3/kms.tf
@@ -31,20 +31,7 @@ resource "aws_kms_key_policy" "transfer_family_key_policy" {
         Principal = {
           AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
         }
-        Action = [
-          "kms:Create*",
-          "kms:Describe*",
-          "kms:Enable*",
-          "kms:List*",
-          "kms:Put*",
-          "kms:Update*",
-          "kms:Revoke*",
-          "kms:Disable*",
-          "kms:Get*",
-          "kms:Delete*",
-          "kms:ScheduleKeyDeletion",
-          "kms:CancelKeyDeletion"
-        ]
+        Action   = "kms:*"
         Resource = aws_kms_key.transfer_family_key.arn
       },
       {
