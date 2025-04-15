8d9b37cef21bfb59e94c99dee3e7e0772fde10ab
prabirsekhri <prabirsekhri@gmail.com>
2025-04-15 09:40:12 -0400

    moved KMS block to main.tf

--- examples/sftp-public-endpoint-service-managed-S3/main.tf
+++ examples/sftp-public-endpoint-service-managed-S3/main.tf
@@ -80,4 +80,57 @@ module "s3_bucket" {
   }
 }
 
-# KMS resources moved to kms.tf
+###################################################################
+# KMS key and policies for Transfer Server
+###################################################################
+
+# KMS Key resource
+resource "aws_kms_key" "transfer_family_key" {
+  description             = "KMS key for encrypting S3 bucket and cloudwatch log group"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+
+  tags = {
+    Purpose = "Transfer Family Encryption"
+  }
+}
+
+# KMS Key Alias
+resource "aws_kms_alias" "transfer_family_key_alias" {
+  name          = "alias/transfer-family-key"
+  target_key_id = aws_kms_key.transfer_family_key.key_id
+}
+
+# KMS Key Policy
+resource "aws_kms_key_policy" "transfer_family_key_policy" {
+  key_id = aws_kms_key.transfer_family_key.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "Enable Limited Admin Permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = aws_kms_key.transfer_family_key.arn
+      },
+      {
+        Sid    = "Allow CloudWatch Logs"
+        Effect = "Allow"
+        Principal = {
+          Service = "logs.${var.aws_region}.amazonaws.com"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:Describe*"
+        ]
+        Resource = aws_kms_key.transfer_family_key.arn
+      }
+    ]
+  })
+}
